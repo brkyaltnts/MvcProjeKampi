3b74ff03ef9f74b643ebc6ddbdf6aae0d97e27f4
Berkay <brky_altnts@outlook.com>
2021-04-24 01:59:03 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        //CRUD (Create-Read-Update-Delete)
+        List<T> List();
+        void Insert(T p);
+        void Delete(T p);
+        void Update(T p);
+
+        // Şartlı Listeleme 
+        List<T> List(Expression<Func<T, bool>> filter);
+    }
+}
